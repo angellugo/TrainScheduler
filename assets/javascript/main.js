8914eac689b38457ac7443b7c61ed32bd6d7e86f
Angel Lugo <this@angellugo.com>
2018-06-16 09:07:46 -0700

    Initial setup of the files with bootstrap and firebase

---
+++ assets/javascript/main.js
@@ -0,0 +1,11 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB3qWy3kAjXkye5Ub2oXiH5ZJWZlpP0VKI",
+    authDomain: "trainscheduler-4eb7e.firebaseapp.com",
+    databaseURL: "https://trainscheduler-4eb7e.firebaseio.com",
+    projectId: "trainscheduler-4eb7e",
+    storageBucket: "",
+    messagingSenderId: "117899904334"
+  };
+  firebase.initializeApp(config);
